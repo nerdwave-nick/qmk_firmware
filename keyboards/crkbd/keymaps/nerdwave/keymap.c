8b8de6bb1ec20059390b13525b1ba0fc1635a4bc
Nick Mueller <muellernick97@gmx.de>
2023-12-31 07:38:14 +0100

    feat: qwerty keymap for crkbd

--- keyboards/crkbd/keymaps/nerdwave/keymap.c
+++ keyboards/crkbd/keymaps/nerdwave/keymap.c
@@ -20,14 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LBASE] = LAYOUT_split_3x6_3(
-  //,--------------------------------------------.                    ,--------------------------------------------.
-    XXXXXXX,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT, XXXXXXX,
-  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-   XXXXXXX, GUI_T(KC_A),   ALT_T(KC_R),    CTL_T(KC_S),   SFT_T(KC_T),    KC_G, /*#####*/ KC_M, SFT_T(KC_N),   CTL_T(KC_E),    ALT_T(KC_I),   GUI_T(KC_O), XXXXXXX,
-  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-    XXXXXXX,      KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-     LT(LMEDIA, KC_ESC), LT(LNAV, KC_SPC), LT(LMOUSE,KC_TAB),   LT(LSYM, KC_ENT), LT(LNUM, KC_BSPC), LT(LFN,KC_DEL)
+       //,--------------------------------------------.                    ,--------------------------------------------.
+XXXXXXX,   KC_Q, KC_W,  KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+       //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+XXXXXXX, GUI_T(KC_A),   ALT_T(KC_S),    CTL_T(KC_D),   SFT_T(KC_F),    KC_G, /*#####*/ KC_H, SFT_T(KC_J),   CTL_T(KC_K),    ALT_T(KC_L),   GUI_T(KC_QUOT), XXXXXXX,
+       //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+XXXXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+       //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+      LT(LMEDIA, KC_ESC), LT(LNAV, KC_SPC), LT(LMOUSE,KC_TAB),   LT(LSYM, KC_ENT), LT(LNUM, KC_BSPC), LT(LFN,KC_DEL)
                           //|-----------------------------|  |-----------------------------|
 
   ),
